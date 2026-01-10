0fbdc3a2df57c72c9684c747e04d3e0df6bbf7bf
csbb4214 <csbb4214@student.uibk.ac.at>
2026-01-10 21:50:57 +0100

    set areas through env-variable for more efficient deployment changes

--- src/cloud/src/main/java/orchestrator/OrchestratorGuardian.java
+++ src/cloud/src/main/java/orchestrator/OrchestratorGuardian.java
@@ -8,16 +8,18 @@ import orchestrator.analysis.MLInferenceClient;
 import orchestrator.ingest.NatsIngestActor;
 import orchestrator.risk.RiskAssessmentActor;
 
+import java.util.Map;
+
 public class OrchestratorGuardian extends AbstractBehavior<Void> {
 
     // --------------------
     // Factory
     // --------------------
-    public static Behavior<Void> create(String natsUrl, String mlUrl) {
-        return Behaviors.setup(ctx -> new OrchestratorGuardian(ctx, natsUrl, mlUrl));
+    public static Behavior<Void> create(String natsUrl, String mlUrl, Map<String, double[]> areaCoords) {
+        return Behaviors.setup(ctx -> new OrchestratorGuardian(ctx, natsUrl, mlUrl, areaCoords));
     }
 
-    private OrchestratorGuardian(ActorContext<Void> context, String natsUrl, String mlUrl) {
+    private OrchestratorGuardian(ActorContext<Void> context, String natsUrl, String mlUrl, Map<String, double[]> areaCoords) {
         super(context);
 
         // create client for communication with ML model
@@ -27,7 +29,7 @@ public class OrchestratorGuardian extends AbstractBehavior<Void> {
         ActorRef<AlertPublisherActor.Command> alertPublisher =
                 context.spawn(AlertPublisherActor.create(natsUrl), "alertPublisher");
         ActorRef<RiskAssessmentActor.Command> riskAssessment =
-                context.spawn(RiskAssessmentActor.create(alertPublisher), "riskAssessment");
+                context.spawn(RiskAssessmentActor.create(alertPublisher, areaCoords), "riskAssessment");
         ActorRef<FireAnalysisActor.Command> fireAnalysis =
                 context.spawn(FireAnalysisActor.create(riskAssessment, alertPublisher, mlClient), "fireAnalysis");
         context.spawn(NatsIngestActor.create(natsUrl, fireAnalysis), "natsIngest");
